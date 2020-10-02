fe298c8c195aa2eb457b7fadcd5a08310df4fe8c
Vedanth P <vedpadu@gmail.com>
2020-10-02 15:21:52 -0700

    added the boss sprites with the hands

---
+++ Assets/meshLayerSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshLayerSet : MonoBehaviour
+{
+    public string layerName;
+
+    public int layerInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().sortingLayerName = layerName;
+        GetComponent<MeshRenderer>().sortingOrder = layerInt;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
