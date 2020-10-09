7836a6bdd4ab8fc1c6ef0b0d6033b436ea8f3c7a
Vedanth P <vedpadu@gmail.com>
2020-10-09 11:55:11 -0700

    main menu anims

---
+++ Assets/sceneLoaderButtonScript.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneLoaderButtonScript : MonoBehaviour
+{
+    public float scaleHovered;
+
+    public float lerpValueButton;
+    private float currentScale;
+
+    private bool mouseEntered;
+
+    private float initScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScale = transform.localScale.x;
+        initScale = currentScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(currentScale, currentScale, 1f);
+        if (mouseEntered)
+        {
+            currentScale = Mathf.Lerp(currentScale, scaleHovered, lerpValueButton);
+        }
+        else
+        {
+            currentScale = Mathf.Lerp(currentScale, initScale, lerpValueButton);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseEntered = true;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseEntered = false;
+    }
+}
